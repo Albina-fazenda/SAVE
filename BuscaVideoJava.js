38a3bfb36291bce06ccf958e470bff66bbfcec70
Albina-fazenda <61841238+Albina-fazenda@users.noreply.github.com>
2020-07-01 00:47:07 -0300

    Update BuscaVideoJava.js

--- BuscaVideoJava.js
+++ BuscaVideoJava.js
@@ -337,3 +337,30 @@ function criaArray (data) {
   };
 
 };
+
+function initClient() {
+    gapi.client.init({
+        // Ваш ключ API
+        apiKey: AIzaSyBjkycJDy1Z2un7cb4-EbwptFrg4oVxOQE,
+
+        // Ваш идентификатор клиента
+        clientId: 378238551546-ue2tk6v8fnau2n2mtq98q5b3iu0otqd9.apps.googleusercontent.com,
+
+        // Указание, что мы хотим использовать Google Drive API v3
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+
+        // Запрос доступа к application data folder (см. ниже)
+        scope: 'https://www.googleapis.com/auth/drive.appfolder'
+
+    }).then(() => {
+        // Начинаем ловить события логина/логаута (см. ниже)
+        gapi.auth2.getAuthInstance().isSignedIn.listen(onSignIn)
+        // инициализация приложения
+        initApp()
+
+    }, error => {
+        console.log('Failed to init GAPI client', error)
+        // работаем без гугла
+        initApp({showAlert: 'google-init-failed-alert'})
+    })
+}
